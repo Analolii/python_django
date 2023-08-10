2c53b273bc842b875995ec810d21cde3048e9551
Анатолий Романюк <romanukaa89@gmail.com>
2023-08-10 22:50:07 +0700

    Added 'Prints'

--- M_05_RequestsAndMiddlewares/mysite/requestdataapp/middlewares.py
+++ M_05_RequestsAndMiddlewares/mysite/requestdataapp/middlewares.py
@@ -12,11 +12,19 @@ class TimeRequestsMiddleware:
         update_frequency = 15
         ip = request.META['REMOTE_ADDR']
         request_time = round(time.time(), 0)
-
+        print('Исходные данные хранилища адресов клиентов', self.ip_time)
+        print('Поступил запрос')
+        print('Время запроса', request_time)
+        print('IP адрес клиента', ip)
+        print('=======================')
         if request_time - self.ip_time.get(ip, 0) > update_frequency:
+            print('Запрос успешно обработан, прошло более 15 секунд')
             self.ip_time[ip] = request_time
             response = self.get_response(request)
-        else:
-            response = HttpResponse('Request limit exceeded', status=400)
 
+        else:
+            print('Ошибка! Запрос не обработан. Прошло менее 15 секунд.')
+            response = HttpResponse('Request limit exceeded', status=400)
+        print('Данные хранилища адресов после обработки запроса', self.ip_time)
+        print('~~~~~~~~~~~~~~~~~~~~~')
         return response
