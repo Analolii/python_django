5fe55444607d1d0bcd6942882477385df2cff07f
Анатолий Романюк <romanukaa89@gmail.com>
2024-05-17 20:19:04 +0700

    completed the task for module 18

--- import_export/mysite/mysite/urls.py
+++ import_export/mysite/mysite/urls.py
@@ -16,12 +16,24 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.sitemaps.views import sitemap
+from .sitemaps import sitemaps
+
 from django.urls import path, include
+
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('shop/', include('shopapp.urls')),
     path('myauth/', include('myauth.urls')),
+    path(
+        "sitemap.xml",
+        sitemap,
+        {"sitemaps": sitemaps},
+        name="django.contrib.sitemaps.views.sitemap",
+
+    )
 ]
 
 if settings.DEBUG:
